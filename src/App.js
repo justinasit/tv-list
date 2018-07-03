31c82fdb1d39d2901a282c314465f0896a005b5a
Justinas Teresius <justinast@gmail.com>
2018-07-03 19:53:22 +0100

    Testing out movie api using fetch

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://api.themoviedb.org/3/search/movie?api_key=b3487e6f673fc1e8d1fcbfa4feef3fb8&query=Groundhog')
+    .then(results => {
+      return results.json();
+    }).then(data => {
+      this.setState({name: data.results[0].original_title});
+      console.log(data);
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Your TV List</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          { this.state.name }
         </p>
       </div>
     );
