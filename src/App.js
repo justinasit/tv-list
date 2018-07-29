62d86ff236d3d91d526fbeba60335617178a6c12
Justinas Teresius <justinast@gmail.com>
2018-07-29 16:35:42 +0100

    Storing watched seasons in local storage

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ class App extends Component {
       this.state.showIds = JSON.parse(localStorage.getItem('showIds'));
     }
     let shows = [];
-    this.state.showIds.map((id) => 
-      Tmdb.getInfoById(id).then(data => {
+    this.state.showIds.map((show) => 
+      Tmdb.getInfoById(show.id).then(data => {
         shows.push({name: data.name, number_of_seasons: data.number_of_seasons});
         this.setState({
           myShows: shows
@@ -41,25 +41,39 @@ class App extends Component {
     });
   }
 
+  addShowCheck = (id) => {
+    if (!this.state.showIds.map(show => show.id).includes(id)) {
+      this.addShow(id);
+    }
+  }
+
   addShow = (id) => {
-    if (!this.state.showIds.includes(id)) {
-      this.state.showIds.push(id);
+    this.state.showIds.push({id: id, seasons_watched: []});
       localStorage.setItem('showIds', JSON.stringify(this.state.showIds));
       Tmdb.getInfoById(id).then(data => {
         this.state.myShows.push({name: data.name, number_of_seasons: data.number_of_seasons});
         this.setState({
           myShows: this.state.myShows,
-        });
-      })
-    }
+      });
+    })
   }
 
-  listSeasons = (numberOfSeasons) => {
+  listSeasons = (numberOfSeasons, showIndex) => {
     let rows = [];
     for (var i = 1; i <= numberOfSeasons; i++) {
-        rows.push(<span key={i}>Season {i} <input type="checkbox"/><br/></span>);
+        rows.push(<span key={i}>Season {i} <input onClick={this.checkSeason.bind(this, i, showIndex)} type="checkbox"/><br/></span>);
     }
+    console.log(rows);
     return <span>{rows}</span>;
+  }
+
+  checkSeason = (seasonIndex, showIndex) => {
+    if (this.state.showIds[showIndex].seasons_watched.includes(seasonIndex)) {
+      this.state.showIds[showIndex].seasons_watched.splice(seasonIndex);
+    } else {
+      this.state.showIds[showIndex].seasons_watched.push(seasonIndex);
+    }
+    localStorage.setItem('showIds', JSON.stringify(this.state.showIds));
   }
 
   render() {
@@ -72,7 +86,7 @@ class App extends Component {
             <p key={index}>
               <li>{item.name}
                 <br/><br />
-                { this.listSeasons(item.number_of_seasons) }
+                { this.listSeasons(item.number_of_seasons, index) }
               </li>
         </p>)}
           <form className="App-intro" onSubmit={this.onSubmit}>
@@ -82,7 +96,7 @@ class App extends Component {
           { this.state.items.map((item, index) => 
             <p key={index}>
               <li>{item.original_name}</li>
-              <button onClick={() => this.addShow(item.id)}>Add</button>
+              <button onClick={() => this.addShowCheck(item.id)}>Add</button>
             </p>) }
       </div>
     );
