669463d7c1425a8e10e99b4d3b9ad36cef1839ba
Justinas Teresius <justinast@gmail.com>
2018-12-27 17:05:57 +0200

    Listing archived shows in archived page

--- src/components/Archived.js
+++ src/components/Archived.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import Storage from '../Storage';
+import { Button } from 'reactstrap';
 
-const Archived = () => (
-  <p>Hello!</p>
-)
+export default class ListResults extends React.Component {
   
-export default Archived;
+  state = {
+    archivedShows: [],
+  };
+  storage = new Storage();
+
+  componentDidMount() {
+    this.setState({
+      archivedShows: this.storage.getItem('archivedShows')
+    });
+  }
+
+  render() {
+    return ( 
+      <div>
+        <h2>Archived Shows</h2><br/>
+        { (this.state.archivedShows.length === 0) ? <p>Nothing here!</p> : ''}
+        { this.state.archivedShows.map((item, index) => 
+            <p key={index}> 
+              <li>{`${item.name} (Seasons - ${item.number_of_seasons})`} 
+              <Button size="sm" id={'unarchive-button-'+index} onClick={(e) => this.unArchiveShow(e, 'active', item.id, index)}>Un-Archive</Button>
+                <br/><br />  
+              </li>
+        </p>)}
+      </div>
+    )
+  }
+}
