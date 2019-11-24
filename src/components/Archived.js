9de5665b3f2083e6f0ce7098412fe20ed10886db
Justinas Teresius <justinast@gmail.com>
2019-11-24 18:10:43 +0000

    Converted Archived to a functional component

--- src/components/Archived.js
+++ src/components/Archived.js
@@ -1,46 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Storage from '../Storage';
 import { Button } from 'reactstrap';
 
-export default class ListResults extends React.Component {
-  
-  state = {
-    archivedShows: [],
-  };
-  storage = new Storage();
+function Archived() {
+  const storage = new Storage();
+  const [archivedShows, setArchivedShows] = useState(storage.getItem('archivedShows') ? storage.getItem('archivedShows') : []);
 
-  componentDidMount() {
-    this.setState({
-      archivedShows: this.storage.getItem('archivedShows') ? this.storage.getItem('archivedShows') : []
-    });
-  }
-
-  unArchiveShow(id, index) {
-    const storedShows = this.storage.getItem('storedShows');
+  const unArchiveShow = (id, index) => {
+    const storedShows = storage.getItem('storedShows');
     storedShows.push({id: id, seasons_watched: []});
-    this.storage.setItem('storedShows', storedShows);
+    storage.setItem('storedShows', storedShows);
     
-    const archivedShows = this.state.archivedShows;
-    archivedShows.splice(index, 1);
-    this.setState({
-      archivedShows: archivedShows
-    });
-    this.storage.setItem('archivedShows', archivedShows);
+    const showsWithoutRemovedItem = archivedShows.filter((show, key) => key !== index);
+    setArchivedShows(showsWithoutRemovedItem);
+    storage.setItem('archivedShows', showsWithoutRemovedItem);
   }
 
-  render() {
-    return ( 
-      <div>
-        <h2>Archived Shows</h2><br/>
-        { (this.state.archivedShows.length === 0) ? <p>Nothing here!</p> : ''}
-        { this.state.archivedShows.map((item, index) => 
-            <p key={index}> 
-              <li>{`${item.name} (Seasons - ${item.number_of_seasons})`} 
-              <Button size="sm" id={'unarchive-button-'+index} onClick={(e) => this.unArchiveShow(item.id, index)}>Un-Archive</Button>
-                <br/><br />  
-              </li>
-        </p>)}
-      </div>
-    )
-  }
+  return ( 
+    <div>
+      <h2>Archived Shows</h2><br/>
+      { (archivedShows.length === 0) ? <p>Nothing here!</p> : ''}
+      { archivedShows.map((item, index) => 
+          <p key={index}> 
+            <li>{`${item.name} (Seasons - ${item.number_of_seasons})`} 
+            <Button size="sm" id={'unarchive-button-'+index} onClick={(e) => unArchiveShow(item.id, index)}>Un-Archive</Button>
+              <br/><br />  
+            </li>
+      </p>)}
+    </div>
+  )
 }
+
+export default Archived;
