05b32ff4076aed170ac041719c6a388052d153c2
Justinas Teresius <justinast@gmail.com>
2020-01-15 15:42:07 +0000

    Updating root reducer to ensure proper state updates

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,21 @@
 import {combineReducers} from 'redux';
 
-const myShows = (state = {}, action)  => action.payload ? action.payload : null;
-const storedShows = (state = {}, action)  => action.payload ? action.payload : null;
+const myShows = (state = {}, action)  => {
+    switch (action.type) {
+        case 'myShows':
+            return action.payload;
+        default:
+            return state;
+    }
+};
+const storedShows = (state = {}, action)  => {
+    switch (action.type) {
+        case 'storedShows':
+            return action.payload;
+        default:
+            return state;
+    }
+};
 
 const rootReducer = combineReducers({
     myShows,
