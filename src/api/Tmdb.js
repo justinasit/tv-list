a6ea6be9f3a83422fbbc21199119776073fc05cd
Justinas Teresius <justinast@gmail.com>
2020-01-25 18:50:03 +0000

    Adding TMDB api key to .env

--- src/api/Tmdb.js
+++ src/api/Tmdb.js
@@ -1,4 +1,4 @@
-const apiKey = 'b3487e6f673fc1e8d1fcbfa4feef3fb8';
+const apiKey = process.env.REACT_APP_TMDB_API_KEY;
 
 export function searchTv(term) {
   return fetch(`https://api.themoviedb.org/3/search/tv?api_key=${apiKey}&query="${term}"`).then(
