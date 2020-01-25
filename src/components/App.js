9e2ba652ba36dbc2238de9a91673c58b8a4d6d41
Justinas Teresius <justinast@gmail.com>
2020-01-25 18:49:41 +0000

    Configuring dotenv

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import Header from './Header';
 import Main from './Main';
 import '../stylesheets/BaseStyle.css';
 
+require('dotenv').config();
+
 // this component will be rendered by our <___Router>
 const App = () => (
   <div className="App">
