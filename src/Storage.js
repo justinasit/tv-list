fb5c06d2c80e25357933a6f2a750bc614e18b4da
Justinas Teresius <justinast@gmail.com>
2020-02-22 13:13:23 +0000

    Updating storage to fetch from an internal api

--- src/Storage.js
+++ src/Storage.js
@@ -1,9 +1,19 @@
 export default class Storage {
+  storageUrl = process.env.REACT_APP_STORAGE_URL;
+
   setItem = (key, value) => {
     localStorage.setItem(key, JSON.stringify(value));
   };
 
-  getItem = key => {
-    return localStorage.getItem(key) ? JSON.parse(localStorage.getItem(key)) : null;
-  };
+  async getItem(key) {
+    switch (key) {
+      case 'storedShows':
+        const result = await fetch(this.storageUrl + 'shows');
+        return await result.json();
+      case 'archivedShows':
+        return localStorage.getItem(key) ? JSON.parse(localStorage.getItem(key)) : null;
+      default:
+        return '';
+    }
+  }
 }
