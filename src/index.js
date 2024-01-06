2862862336d2a64103ef31bd6dadc87bb57fbef8
Justinas Teresius <justinast@gmail.com>
2024-01-06 18:58:35 +0000

    Allowing redux extension

--- src/index.js
+++ src/index.js
@@ -5,14 +5,21 @@ import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import rootReducer from './reducers';
 import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer = composeEnhancers();
+const store = createStore(rootReducer, enhancer);
 root.render(
-  <Provider store={createStore(rootReducer)}>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
